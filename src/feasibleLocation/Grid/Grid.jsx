c9813ba1e8156cd8a50e6501c8370be509a597ed
astha1411 <thakurastha2@gmail.com>
2021-08-14 14:03:28 +0530

    Grid and Square modified

---
+++ src/feasibleLocation/Grid/Grid.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Square from "./Square";
+import { render } from 'react-dom';
+class Grid extends React.Component{
+
+    render(){
+        return(
+            <table className="grid">
+              
+            </table>
+        );
+        
+    }
+    
+}
+export default Grid;
